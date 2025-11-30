0a9175cae088baa9cdb7927331bc7c89c0866f8c
Fleurelle <r.d.jeanlouis01@gmail.com>
2025-11-30 13:53:04 -0500

    Error handling in signup form

--- user_accounts/forms.py
+++ user_accounts/forms.py
@@ -1,19 +1,46 @@
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'password')
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         # Make all fields required
-        for field in ('first_name', 'last_name', 'username', 'password'):
+        for field in ('first_name', 'last_name', 'username', 'password1', 'password2'):
             self.fields[field].required = True
             
         # Customize field labels
         self.fields['first_name'].label = 'First Name'
         self.fields['last_name'].label = 'Last Name'
+        self.fields['password1'].label = 'Password'
+        self.fields['password2'].label = 'Confirm Password'
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username is already taken. Please choose another.")
+        return username
+
+    def clean_password2(self):
+            password1 = self.cleaned_data.get('password1')
+            password2 = self.cleaned_data.get('password2')
+            
+            if password1 and password2 and password1 != password2:
+                raise ValidationError("Passwords don't match. Please try again.")
+            
+            return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
+    

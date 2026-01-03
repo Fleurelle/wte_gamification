0a5772d873e935e6cb65bbdd6d5eab9c75299b2e
Fleurelle <r.d.jeanlouis01@gmail.com>
2026-01-03 11:45:15 -0500

    TEST 2 - DEBUG

--- wte/settings.py
+++ wte/settings.py
@@ -38,6 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 DEBUG = 'RENDER' not in os.environ
+print(f'DEBUG={DEBUG}')
 
 ALLOWED_HOSTS = [
     'wte-gamification.onrender.com'
